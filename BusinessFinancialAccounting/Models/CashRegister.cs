d6f9ff8bee70dc94693233dc7d2601a28e826012
L3gvccy <oleksandr_iv@knu.ua>
2025-09-30 22:44:59 +0300

    Add User and CashRegister

---
+++ BusinessFinancialAccounting/Models/CashRegister.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessFinancialAccounting.Models
+{
+    public class CashRegister
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public User User { get; set; }
+        public int CashBalance { get; set; }
+        public int CardBalance { get; set; }
+    }
+}
---
+++ BusinessFinancialAccounting/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessFinancialAccounting.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public CashRegister? CashRegister { get; set; }
+
+    }
+}
