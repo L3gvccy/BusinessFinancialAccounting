eee479aa234d4b37f4fb3fe896d5ce4f91dea42d
Arsen Altunyan <arsenaltunyan@knu.ua>
2025-10-01 20:56:52 +0300

    Add registration

---
+++ BusinessFinancialAccounting/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessFinancialAccounting.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessFinancialAccounting.Controllers
+{
+    public class AccountController : Controller
+    {
+        private AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View(user);
+            }
+
+            if (_context.Users.Any(u => u.Username == user.Username))
+            {
+                ModelState.AddModelError("", "Такий логін вже існує!");
+                return View(user);
+            }
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return RedirectToAction("Login");
+        }
+    }
+}
