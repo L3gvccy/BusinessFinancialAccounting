348ada975cc04e3fb78016d84160f7b6240287bc
ArsenAltunyan-alt <arsenaltunyan@knu.ua>
2025-12-01 16:55:37 +0200

    Lab 6 SonarQube + Grafan + Ptometheus + ElasticSearch

---
+++ BusinessFinancialAccounting/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BusinessFinancialAccounting.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DemoController : ControllerBase
+{
+    private static readonly ActivitySource ActivitySource =
+        new("BusinessFinancialAccounting");
+
+    [HttpGet("long-operation")]
+    public async Task<IActionResult> LongOperation()
+    {
+        using var activity = ActivitySource.StartActivity("LongOperation");
+
+        activity?.SetTag("operation.type", "demo-long");
+        activity?.SetTag("user.demo", "test-user");
+        activity?.SetTag("simulated", true);
+
+        await Task.Delay(5000);
+
+        activity?.AddEvent(new ActivityEvent("finished-work"));
+
+        return Ok(new { message = "Long operation done" });
+    }
+}
